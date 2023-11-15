94d5620e6f6bb6ed4988ae681adbda7edbae7c81
Jpcapunay <jeanpierrecapunay17@hotmail.com>
2023-11-15 14:11:54 -0500

    user.js completed for controllers, routes and models

---
+++ routes/user.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as controllers from "./budget.js";
+
+const router = Router();
+
+router.post("/sign-up", controllers.signUp);
+router.post("/sign-in", controllers.signIn);
+router.get("/verify", controllers.verify);
+// router.post("/change-password", controllers.changePassword);
